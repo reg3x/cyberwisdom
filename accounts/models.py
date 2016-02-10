a4a8f7607e914d0a42af142b59416e687634f834
reg3x <diegoaguilera85@gmail.com>
2016-02-09 21:11:14 -0430

    adding model for profiles

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,18 @@
-from django.db import models
+from __future__ import unicode_literals
 
-# Create your models here.
+from django.db import models
+from django.contrib.auth.models import User
+from django.utils.translation import ugettext as _
+from userena.models import UserenaBaseProfile
+
+
+class MyProfile(UserenaBaseProfile):
+    user = models.OneToOneField(User,
+                                unique=True,
+                                verbose_name=_('user'),
+                                related_name='my_profile')
+    userId = models.CharField(max_length=12)
+    age = models.IntegerField(null=True, blank=True)
+    location = models.CharField(max_length=40, blank=True)
+    profession = models.CharField(max_length=30, blank=True)
+    description = models.CharField(max_length=100, blank=True)
