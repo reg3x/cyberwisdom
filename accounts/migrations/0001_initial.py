5b6478021846d8ba22b0b858ac5db0c49b2fd7b1
reg3x <diegoaguilera85@gmail.com>
2016-02-09 21:19:16 -0430

    static files

---
+++ accounts/migrations/0001_initial.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+from django.conf import settings
+import easy_thumbnails.fields
+import userena.models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='MyProfile',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('mugshot', easy_thumbnails.fields.ThumbnailerImageField(help_text='A personal image displayed in your profile.', upload_to=userena.models.upload_to_mugshot, verbose_name='mugshot', blank=True)),
+                ('privacy', models.CharField(default=b'registered', help_text='Designates who can view your profile.', max_length=15, verbose_name='privacy', choices=[(b'open', 'Open'), (b'registered', 'Registered'), (b'closed', 'Closed')])),
+                ('userId', models.CharField(max_length=12)),
+                ('age', models.IntegerField(null=True, blank=True)),
+                ('location', models.CharField(max_length=40, blank=True)),
+                ('profession', models.CharField(max_length=30, blank=True)),
+                ('description', models.CharField(max_length=100, blank=True)),
+                ('user', models.OneToOneField(related_name='my_profile', verbose_name='user', to=settings.AUTH_USER_MODEL)),
+            ],
+            options={
+                'abstract': False,
+                'permissions': (('view_profile', 'Can view profile'),),
+            },
+        ),
+    ]
