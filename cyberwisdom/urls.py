f6d3780c3fc6593289bd9b5978d7831058066c90
reg3x <diegoaguilera85@gmail.com>
2016-02-09 21:22:59 -0430

    setting urls

--- cyberwisdom/urls.py
+++ cyberwisdom/urls.py
@@ -1,10 +1,10 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.views.generic.base import RedirectView
+from userena.views import ProfileListView
 
 urlpatterns = [
-    # Examples:
-    # url(r'^$', 'cyberwisdom.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^accounts/', include('userena.urls')),
+    url(r'^$', RedirectView.as_view(url='accounts/signin/', permanent=False), name='index'),
 ]
